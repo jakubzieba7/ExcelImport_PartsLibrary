93878263acfa0a9c9afeaefe8fd6d6b1c1bea0d9
Jakub Zięba <jakubzieba7@gmail.com>
2023-03-12 23:10:16 +0100

    PartsComparison Form

---
+++ ExcelImport_PartsLibrary/PartsComparison.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SNPlugin
+{
+    partial class PartsComparison
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPartsComparison = new System.Windows.Forms.DataGridView();
+            this.bLoadPartsToSN = new System.Windows.Forms.Button();
+            this.bRefreshPartsList = new System.Windows.Forms.Button();
+            this.bDeletePart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPartsComparison)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPartsComparison
+            // 
+            this.dgvPartsComparison.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPartsComparison.Location = new System.Drawing.Point(-1, 94);
+            this.dgvPartsComparison.Name = "dgvPartsComparison";
+            this.dgvPartsComparison.Size = new System.Drawing.Size(508, 354);
+            this.dgvPartsComparison.TabIndex = 0;
+            // 
+            // bLoadPartsToSN
+            // 
+            this.bLoadPartsToSN.Location = new System.Drawing.Point(352, 34);
+            this.bLoadPartsToSN.Name = "bLoadPartsToSN";
+            this.bLoadPartsToSN.Size = new System.Drawing.Size(90, 23);
+            this.bLoadPartsToSN.TabIndex = 1;
+            this.bLoadPartsToSN.Text = "Załaduj do SN";
+            this.bLoadPartsToSN.UseVisualStyleBackColor = true;
+            this.bLoadPartsToSN.Click += new System.EventHandler(this.bLoadPartsToSN_Click);
+            // 
+            // bRefreshPartsList
+            // 
+            this.bRefreshPartsList.Location = new System.Drawing.Point(198, 34);
+            this.bRefreshPartsList.Name = "bRefreshPartsList";
+            this.bRefreshPartsList.Size = new System.Drawing.Size(75, 23);
+            this.bRefreshPartsList.TabIndex = 2;
+            this.bRefreshPartsList.Text = "Odśwież";
+            this.bRefreshPartsList.UseVisualStyleBackColor = true;
+            this.bRefreshPartsList.Click += new System.EventHandler(this.bRefreshPartsList_Click);
+            // 
+            // bDeletePart
+            // 
+            this.bDeletePart.Location = new System.Drawing.Point(39, 34);
+            this.bDeletePart.Name = "bDeletePart";
+            this.bDeletePart.Size = new System.Drawing.Size(75, 23);
+            this.bDeletePart.TabIndex = 3;
+            this.bDeletePart.Text = "Usuń";
+            this.bDeletePart.UseVisualStyleBackColor = true;
+            this.bDeletePart.Click += new System.EventHandler(this.bDeletePart_Click);
+            // 
+            // PartsComparison
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 450);
+            this.Controls.Add(this.bDeletePart);
+            this.Controls.Add(this.bRefreshPartsList);
+            this.Controls.Add(this.bLoadPartsToSN);
+            this.Controls.Add(this.dgvPartsComparison);
+            this.Name = "PartsComparison";
+            this.Text = "PartComparison";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPartsComparison)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPartsComparison;
+        private System.Windows.Forms.Button bLoadPartsToSN;
+        private System.Windows.Forms.Button bRefreshPartsList;
+        private System.Windows.Forms.Button bDeletePart;
+    }
+}
---
+++ ExcelImport_PartsLibrary/PartsComparison.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SNPlugin
+{
+    public partial class PartsComparison : Form
+    {
+        public int Id { get; set; }
+        public string PartName { get; set; }
+        public string Path { get; set; }
+        public int Quantity { get; set; }
+
+        List<PartExcel> newPartExcelList = new List<PartExcel> { };
+        List<PartLibrary> newPartLibraryList = new List<PartLibrary>();
+        List<PartsComparison> partsComparedList = new List<PartsComparison>();
+
+        public PartsComparison()
+        {
+
+        }
+        public PartsComparison(List<PartExcel> partsExcelList, List<PartLibrary> partsLibraryList)
+        {
+
+            InitializeComponent();
+            newPartLibraryList = partsLibraryList.Where(x => partsExcelList.Any(y => y.Name == x.Name)).OrderBy(x => x.Name).Cast<PartLibrary>().ToList();
+            dgvPartsComparison.DataSource = partsLibraryList.Where(x => partsExcelList.Any(y => y.Name == x.Name)).OrderBy(x => x.Name).Cast<PartLibrary>().ToList();
+
+            int indexer = 0;
+           
+            newPartExcelList = partsExcelList;
+            for (int i = 0; i < newPartLibraryList.Count; i++)
+            {
+                for (int j = 0; j < newPartExcelList.Count; j++)
+                {
+                    indexer++;
+                    if (newPartLibraryList[i].Name == newPartExcelList[j].Name)
+                    {
+                        partsComparedList.Add(new PartsComparison()
+                        {
+                            Id = indexer,
+                            PartName = newPartLibraryList[i].Name,
+                            Path = newPartLibraryList[i].Path,
+                            Quantity = newPartExcelList[j].Quantity,
+                        });
+                        MessageBox.Show(newPartLibraryList[i].Name.ToString() + newPartExcelList[j].Quantity.ToString());
+                        MessageBox.Show(partsComparedList[j].Id + " " + partsComparedList[j].PartName + " " + partsComparedList[j].Quantity + " " + partsComparedList[j].Path);
+                    }
+
+                }
+            }
+            //dgvPartsComparison.DataSource = partsComparedList;
+        }
+
+        private void bDeletePart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bRefreshPartsList_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bLoadPartsToSN_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
