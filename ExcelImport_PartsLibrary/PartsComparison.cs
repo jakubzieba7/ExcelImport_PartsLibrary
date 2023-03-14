09f03bdb0e47e8c3a144807a70d1ed75dca11b49
Jakub Zięba <jakubzieba7@gmail.com>
2023-03-14 21:19:26 +0100

    CreateComparedPartsList method

--- ExcelImport_PartsLibrary/PartsComparison.cs
+++ ExcelImport_PartsLibrary/PartsComparison.cs
@@ -17,7 +17,6 @@ namespace SNPlugin
         public string Path { get; set; }
         public int Quantity { get; set; }
 
-        List<PartExcel> newPartExcelList = new List<PartExcel> { };
         List<PartLibrary> newPartLibraryList = new List<PartLibrary>();
         List<PartsComparison> partsComparedList = new List<PartsComparison>();
 
@@ -27,35 +26,18 @@ namespace SNPlugin
         }
         public PartsComparison(List<PartExcel> partsExcelList, List<PartLibrary> partsLibraryList)
         {
-
             InitializeComponent();
+            dgvPartsComparison.DataSource = CreateComparedPartsList(partsLibraryList, partsExcelList);
+        }
+
+        private List<PartsComparison> CreateComparedPartsList(List<PartLibrary> partsLibraryList, List<PartExcel> partsExcelList)
+        {
+            int indexer = 1;
             newPartLibraryList = partsLibraryList.Where(x => partsExcelList.Any(y => y.Name == x.Name)).OrderBy(x => x.Name).Cast<PartLibrary>().ToList();
-            dgvPartsComparison.DataSource = partsLibraryList.Where(x => partsExcelList.Any(y => y.Name == x.Name)).OrderBy(x => x.Name).Cast<PartLibrary>().ToList();
+            partsComparedList = newPartLibraryList.Join(partsExcelList, pL => pL.Name, pE => pE.Name, (pL, pE) => new { pL.Id, pL.Name, pL.Path, pE.Quantity }).Select(x => new PartsComparison { Id = indexer++, PartName = x.Name, Path = x.Path, Quantity = x.Quantity })
+                .ToList();
 
-            int indexer = 0;
-           
-            newPartExcelList = partsExcelList;
-            for (int i = 0; i < newPartLibraryList.Count; i++)
-            {
-                for (int j = 0; j < newPartExcelList.Count; j++)
-                {
-                    indexer++;
-                    if (newPartLibraryList[i].Name == newPartExcelList[j].Name)
-                    {
-                        partsComparedList.Add(new PartsComparison()
-                        {
-                            Id = indexer,
-                            PartName = newPartLibraryList[i].Name,
-                            Path = newPartLibraryList[i].Path,
-                            Quantity = newPartExcelList[j].Quantity,
-                        });
-                        MessageBox.Show(newPartLibraryList[i].Name.ToString() + newPartExcelList[j].Quantity.ToString());
-                        MessageBox.Show(partsComparedList[j].Id + " " + partsComparedList[j].PartName + " " + partsComparedList[j].Quantity + " " + partsComparedList[j].Path);
-                    }
-
-                }
-            }
-            //dgvPartsComparison.DataSource = partsComparedList;
+            return partsComparedList;
         }
 
         private void bDeletePart_Click(object sender, EventArgs e)
